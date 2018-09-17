5894a4345fc37140ead9e324bae510b3707dec8f
tawita <5910110091@psu.ac.th>
2018-09-17 11:49:03 +0700

    สร้าง wordcard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import WordCard from './CharacterCard';
 import CharacterCard from './CharacterCard';
 
 const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      {
-        Array.from(word).map((c,i) => <CharacterCard value={c} key={i}/>)
-      }
-      </div>
+    <div>
+    <WordCard value="hello"/>
+    </div>
     );
-  }
+   }
+   
 }
+
 export default App;
