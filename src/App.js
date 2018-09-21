03615622a9185d1e224accc5fd83e9746f2eb919
tawita <5910110091@psu.ac.th>
2018-09-21 13:31:07 +0700

    Handler as Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import WordCard from './CharacterCard';
-
+import WordCard from './WordCard';
+const word="Hello";
 class App extends Component {
   render() {
     return (
-    <div>
-    <WordCard value="hello"/>
+    <div className="activeCard">
+    <WordCard value={word}/>
     </div>
     );
    }
