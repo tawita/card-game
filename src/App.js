0fabdbe61fae520f3865657fec6d8a927dd32cd4
tawita <5910110091@psu.ac.th>
2018-09-21 16:34:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import TawitaAsk from './TawitaAsk';
 
 const word = "HELLO";
+const Tawita = "Computer"
 class App extends Component {
   render() {
     return (
     <div className="activecard">
     <WordCard value={word}/>
+    <TawitaAsk value = {Tawita}/>
     </div>
     );
    }
